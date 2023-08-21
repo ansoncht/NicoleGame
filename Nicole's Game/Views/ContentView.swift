d0e814b654374fb43baecac1f803589a1aff39b8
Anson Chung <ansoncht@gmail.com>
2023-08-20 23:55:43 -0700

    added question json

--- Nicole's Game/Views/ContentView.swift
+++ Nicole's Game/Views/ContentView.swift
@@ -9,20 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 40) {
-            VStack(spacing:20) {
-                Text("正字刑警")
-                    .lilacTitle()
-                Text("Hi 周小姐 睇吓你幾時拾得寫翻啱啲字 :)")
-                    .foregroundColor(Color("AccentColor"))
-                Text("啱八成就有獎啦！")
-                    .foregroundColor(Color("AccentColor"))
+        NavigationView {
+            VStack(spacing: 40) {
+                VStack(spacing:20) {
+                    Text("正字刑警")
+                        .lilacTitle()
+                    Text("Hi 周小姐 睇吓你幾時拾得寫翻啱啲字 :)")
+                        .foregroundColor(Color("AccentColor"))
+                    Text("啱八成就有獎啦！")
+                        .foregroundColor(Color("AccentColor"))
+                }
+                NavigationLink {
+                    GameView()
+                } label: {
+                    PrimaryButton(text: "快啲㩒啦")
+                }
+                
             }
-            PrimaryButton(text: "快啲㩒啦")
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .edgesIgnoringSafeArea(.all)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .edgesIgnoringSafeArea(.all)
         .background(Color(red: 0.984313725490196, green: 0.9294117647058824, blue: 0.8470588235294118))
+        }
         
     }
 }
