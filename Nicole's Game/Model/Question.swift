80f929928fe5bdf1581510b2efad004800eaca81
Anson Chung <ansoncht@gmail.com>
2023-09-02 23:34:14 -0700

    Added Question Model and moved Questions.json to root of directory

---
+++ Nicole's Game/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Nicole's Game
+//
+//  Created by Anson on 8/28/23.
+//
+
+import Foundation
+
+struct Trivia: Decodable {
+    var results: [Result]
+    
+    struct Result: Decodable, Identifiable {
+        var id: UUID {
+            UUID()
+        }
+        var question: String
+        var correctAnswer: String
+    }
+}
